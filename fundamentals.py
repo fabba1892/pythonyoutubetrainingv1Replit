8e598c6fd339b45ee7a468794b258769f4f93b18
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-05-26 19:57:56 +0000

    Initial commit

---
+++ fundamentals.py
@@ -0,0 +1,96 @@
+# name = "Beau"
+# print(isinstance(name, str))
+
+# number = "20"
+# age = int(number)
+# print(isinstance(age, int))
+
+# items = ["Dog","Ceasar", "Migo", "luca"]
+# print(sorted(items, key=str.lower))
+# print(items)
+
+#tuple   ****************************
+# names = ("luca", "Fabba")
+# names[-1]
+# names.index("luca")
+# len(names)
+# print("luca" in names)
+# print(sorted(names))
+# newTuple = names + ("Sune", "ieta")
+
+#Dictionaries   ***************************
+# dog = {"name": "Migo", "age": 3, "color": "blue"}
+# dog["favorite food"] = "meat"
+# del dog['color']
+# dogCopy = dog.copy()
+# print(dog)
+
+
+#Sets   ***************************
+# set1 = {"luca", "Fabba", "luca"}
+# set2 = {"luca"}
+# print(list(set1))
+
+
+#Functions   *******************************************
+# def Hello(name, age):
+#   print('Hello! ' + name + ", you are " + str(age) + " years old")
+
+# Hello("Luca", 3)
+# def change(value):
+#   value["name"] = "fabba" 
+
+# val = {"name": "luca"}
+# change(val)
+
+# print(val)
+# def hello(name):
+#   print('Hello ' + name + '!')
+#   return name, "fabba", 8
+# print(hello("luca"))
+
+
+# def test():
+#   age = 3      #local variable and outside variables 
+#   print(age)
+
+# print(age)
+# test()
+
+# def talk(phrase):
+#   def say(word):
+#     print(word)
+
+#   words = phrase.split(' ')
+#   for word in words:
+#     say(word)
+    
+# talk(' will speak to luca')
+
+
+# Objects  *********************************************
+# everything in puthon is an object 
+# it has attributes and methods
+
+# items = [1, 2]
+# items.append(3)
+# items.pop()
+# age = 8
+# age = age + 1 
+
+
+# Loops   *******************************
+
+# count = 0 
+# while count < 10:
+#   print (" condition is True")
+#   count += 1
+  
+# print("After the loop ")
+
+# items = ["luca", "fabba", "migo", "ceasar"]
+# for index, item in enumerate(items):
+#   print(index, item)
+
+# for item in range(15):
+#   print(item)
---
+++ main.py
@@ -0,0 +1,46 @@
+import fundamentals
+
+# def get_choices():
+#   player_choice = input("Eneter a choice (rock, paper, scissors): ")
+#   options = ["rock", "paper", "scissors"]
+#   computer_choice = random.choice(options)
+#   choices = {"player": player_choice, "computer": computer_choice}
+#   return choices
+
+
+# def check_win(player, computer):
+#   print(f"You chose {player}, and Computer chose {computer} ")
+#   if player == computer:
+#     return "Its a tie. "
+#   elif player == "rock":
+#     if computer == "scissors":
+#       return "player wins! rock smashes scissors!"
+#     else:
+#       return "computer wins! paper covers rock!"
+#   elif player == "paper":
+#     if computer == "rock":
+#       return "player wins! paper covers rock!"
+#     else:
+#       return "scissors cuts paper! you lose!"
+#   elif player == "scissors":
+#     if computer == "paper":
+#       return "player wins! scissors cuts paper!"
+#     else:
+#       return "rock smashes scissors! you lose!"
+
+# choices = get_choices()
+# result = check_win(choices["player"], choices["computer"])
+# print(result)
+
+
+#rock paper scissors game
+
+
+# age = 25
+  # print(f" you are {age} yedars old")
+    
+# b = 3 
+# if a == b:
+#   print("ok cool")
+# else:
+#   print("")
