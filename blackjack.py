4b33f63619ce259813e2030655754ed97107bddb
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-06-07 19:52:41 +0000

    each elemnt in list is now dictionry

--- blackjack.py
+++ blackjack.py
@@ -2,7 +2,21 @@ import random
 
 cards = []
 suits = ["spades", "clubs", "hearts", "diamonds"]
-ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+ranks = [
+          {"rank": "A", "value": 11}, 
+  {"rank": "2", "value": 2},
+  {"rank": "3", "value": 3},
+  {"rank": "4", "value": 4},
+  {"rank": "5", "value": 5},
+  {"rank": "6", "value": 6},
+  {"rank": "7", "value": 7},
+  {"rank": "8", "value": 8},
+  {"rank": "9", "value": 9},
+  {"rank": "10", "value": 10},
+  {"rank": "J", "value": 10},
+  {"rank": "Q", "value": 10},
+  {"rank": "K", "value": 10},
+  ]
 for suit in suits:
   for rank in ranks:
     cards.append([suit, rank])
@@ -19,7 +33,7 @@ def deal(number):
 
 
 shuffle()
-
+card = deal(1)[0]
 
 
 
