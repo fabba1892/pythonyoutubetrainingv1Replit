7f64514c59232b215c544d4bf225caf56da6dc27
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-06-09 20:36:07 +0000

    check final results play hand and dealer hand

--- blackjack.py
+++ blackjack.py
@@ -113,7 +113,41 @@ class Game:
       print("*" * 30)
       player_hand.display()
       dealer_hand.display()
-      
+
+      if self.check_winner(player_hand, dealer_hand):
+        continue
+    
+      choice = ""
+      while player_hand.get_value() < 21 and choice not in ["s", "stand"]:
+        choice = input("Please choose 'Hit' or 'Stand': ").lower()
+        print()
+        while choice not in ["h", "s", "hit", "stand"]:
+          choice = input("Please enter 'Hit'or 'STand' (or H/S)").lower()
+          print()
+        if choice in ["hit", "h"]:
+          player_hand.add_card(deck.deal())
+          player_hand.display()
+          
+      if self.check_winner(player_hand, dealer_hand):
+        continue
+
+      player_hand_value = player_hand.get_value()
+      dealer_hand_value = dealer_hand.get_value()
+
+      while dealer_hand_value <17:
+        dealer_hand.add_card(deck.deal())
+        dealer_hand_value = dealer_hand.get_value()
+
+      dealer_hand.display(show_all_dealer_cards=True)
+
+      if self.check_winner(player_hand, dealer_hand):
+        continue
+
+      print("Final Results")
+      print("Your hand: ", player_hand_value)
+      print("Dealer's hand: ", dealer_hand_value)
+
+  
   def check_winner(self, player_hand, dealer_hand, game_over=False):
     if not game_over:
       if player_hand.get_value() > 21:
