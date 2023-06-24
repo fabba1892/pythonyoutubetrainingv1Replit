75d3a2f3c7dcbe2e8f40d3a3553c5256c6ab4bcc
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-06-24 18:21:20 +0000

    random numbers as secrets  and used in numpy

--- intermediate.py
+++ intermediate.py
@@ -93,3 +93,60 @@
 # print(person_json) 
 # print(person_json2) 
 
+# /////////////////////////////////////////////////////////////////////
+
+# random number
+
+# import random
+
+# random float in [0,1)
+# a = random.normalvariate(0, 1)
+# print(a)
+
+# shuffle list 
+# a = list("ABCDEFGHI")
+# random.shuffle(a)
+
+# random.seed(1)
+
+# print(random.random())
+# print(random.uniform(1,10))
+# print(random.choice(list("ABCDEFGHI")))
+
+# print('\nRe-seeding with 42...\n')
+# random.seed(42)  # Re-seed
+
+# print(random.random())
+# print(random.uniform(1,10))
+# print(random.choice(list("ABCDEFGHI")))
+
+
+# secrets for passwords etc 
+
+# import secrets 
+
+# # random integer in range [0, n).
+# a = secrets.randbelow(10)
+# print(a)
+
+# # return an integer with k random bits.
+# a = secrets.randbits(5)
+# print(a)
+
+# # choose a random element from a sequence
+# a = secrets.choice(list("ABCDEFGHI"))
+# print(a)
+
+# import numpy as np
+
+# a = np.random.randint(0, 10, (3, 4))
+# print(a)
+
+# randomly shuffle a nd array.
+# only shuffles the array along the first axis of a multi-dimensional array
+# arr = np.array([[1,2,3], [4,5,6], [7,8,9]])
+# print(arr)
+# np.random.shuffle(arr)
+# print(arr)
+
+# ////////////////////////////////////////////////////////////////////////////////
