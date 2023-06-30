053e43c00010d74eed752369a9f953ea10c88bb1
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-06-30 19:03:23 +0000

    contec manager

--- intermediate.py
+++ intermediate.py
@@ -461,3 +461,25 @@
 
 # shallow and deep copying
 
+class ManagedFile:
+  def __init__(self, filename):
+    self.filename = filename
+
+  def __enter__(self):
+    print('enter')
+    self.file = open(self.filename, 'W')
+    return self.file
+
+  def __exit__(self, exc_type, exc_value, exc_traceback):
+    if self.file:
+      self.file.close()
+    if exc_type is not None:
+      print('exception has been handled')
+    print('exc:', exc_type, exc_value)
+    print('exit')
+
+with ManagedFile('notes.txt') as file:
+  print('do some stuff..')
+  file.write('some to do...')
+  file.somemethod()
+print('continuing')
